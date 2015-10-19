923055d9effac7785bcaa06bb7862f7c916cf31e
raydilme <invictus016@hotmail.com>
2015-10-19 16:34:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
 var fs = require('fs');
 var outfile = "homework.txt";
 var out = "A Startup is a business built to grow rapidly.\n";
