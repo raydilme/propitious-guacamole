4dfee7e739fbb5d82265617b7a773f44256229cd
raydilme <invictus016@hotmail.com>
2015-10-19 15:43:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-var express = require('express')
-var app = express()
-
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
-
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+#!/usr/bin/env node
+var fs = require('fs');
+var outfile = "homework.txt";
+var out = "A Startup is a business built to grow rapidly.\n";
+fs.writeFileSync(outfile, out);  
+console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
